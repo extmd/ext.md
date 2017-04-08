f837403a6eedf930dfbb968546b2b9fa2f848cda
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2017-04-08 14:09:56 +0300

    Using a separate route for home

--- site/src/lib/extmd/routing.ts
+++ site/src/lib/extmd/routing.ts
@@ -1,1 +1,9 @@
-// ext.md specific routing
+import * as Router from "koa-router";
+
+export default function(router: Router): Router {
+  router.get("/", async function(ctx: Router.IRouterContext, next: () => Promise<any>){
+    console.info("GET ", ctx.path);
+    ctx.body = "hello home";
+  });
+  return router;
+};
