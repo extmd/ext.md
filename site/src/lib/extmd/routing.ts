202d8ee7199c95fb871fafc184e462335ded7e70
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2017-05-09 14:48:19 +0300

    Updated all versions

--- site/src/lib/extmd/routing.ts
+++ site/src/lib/extmd/routing.ts
@@ -1,9 +1,9 @@
 import * as Router from "koa-router";
 
 export default function(router: Router): Router {
-  router.get("/", async function(ctx: Router.IRouterContext, next: () => Promise<any>){
+  router.get("/", async (ctx: Router.IRouterContext, next: () => Promise<any>) => {
     console.info("GET ", ctx.path);
     ctx.body = ctx.state.render.template("extmd/home");
   });
   return router;
-};
+}
